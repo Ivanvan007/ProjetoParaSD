72cf4e009268abc7b996e3b26a5237a03ee54f75
Ivanvan007 <ivan.ibanhez.vilar@gmail.com>
2024-05-29 23:53:21 +0100

    ProjectNight

--- codigo_Ana/Manager.js
+++ codigo_Ana/Manager.js
@@ -1,9 +1,10 @@
 const Node = require("./codigo_Ana/Node");
+const stack = require(stack);
 
 let numb = 3904;
 let temp = "" + numb;
 let test1 = temp[1];
-let test2 = temp[2,3];
+let test2 = temp.slice(2,3);
 console.log(test1, test2);
 /*
 let test = new Node(3101);
